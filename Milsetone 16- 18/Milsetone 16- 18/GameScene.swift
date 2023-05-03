89e382f9186e744d03b8955da84270514bc35bca
Artem <artchzhen@gmail.com>
2023-05-03 20:58:59 +0900

    added targets, timer, func random createTargets

--- Milsetone 16- 18/Milsetone 16- 18/GameScene.swift
+++ Milsetone 16- 18/Milsetone 16- 18/GameScene.swift
@@ -12,7 +12,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var background: SKSpriteNode!
     var timerLabel: SKLabelNode!
     var scoreLabel: SKLabelNode!
+    var shots: SKSpriteNode!
+    var shotsList = ["shots0", "shots1", "shots2", "shots3"]
+    var shotsIterator = 0 {
+        didSet {
+            shots.removeFromParent()
+            shots = SKSpriteNode(imageNamed: shotsList[shotsIterator])
+            shots.position = CGPoint(x: 64, y: 32)
+            shots.name = "reload"
+            addChild(shots)
+           
+        }
+    }
     
+    let foods = [ "bacon", "banana", "pizza", "popcorn"]
+    var isGameOver = false
+    var gameTimer: Timer?
     
     var score = 0 {
         didSet {
@@ -31,10 +46,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(background)
         
         timerLabel = SKLabelNode(fontNamed: "Chalkduster")
-        timerLabel.text = "60 c"
+        timerLabel.text = "10 c"
         timerLabel.position = CGPoint(x: 512, y: 700)
         timerLabel.zPosition = 105
         addChild(timerLabel)
+        
+        shots = SKSpriteNode(imageNamed: shotsList[shotsIterator])
+        shots.position = CGPoint(x: 64, y: 32)
+        addChild(shots)
     
         scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
         scoreLabel.position = CGPoint(x: 1000, y: 700)
@@ -46,11 +65,31 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = .zero
         physicsWorld.contactDelegate = self
         
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.8, target: self, selector: #selector(createFoods), userInfo: nil, repeats: true)
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.8, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
 
+    }
+    @objc func updateTime() {
+        
     }
         
     override func update(_ currentTime: TimeInterval) {
         
+    }
+    
+  @objc func createFoods() {
+      guard let enemy = foods.randomElement() else { return }
+      
+      let sprite = SKSpriteNode(imageNamed: enemy)
+      sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...800))
+      addChild(sprite)
+        
+      sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+      sprite.physicsBody?.categoryBitMask = 1
+      sprite.physicsBody?.velocity = CGVector(dx: -400, dy: 0)
+      sprite .physicsBody?.angularVelocity = 4
+      sprite.physicsBody?.linearDamping = 0
+      sprite.physicsBody?.angularDamping = 1
     }
     
 }
