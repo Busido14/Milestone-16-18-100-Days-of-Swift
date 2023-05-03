6bd16389dcc5f38c3adbf230c1ccfda0e13c59ad
Artem <artchzhen@gmail.com>
2023-05-03 18:41:14 +0900

    initial commit development

---
+++ Milsetone 16- 18/Milsetone 16- 18/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  Milsetone 16- 18
+//
+//  Created by Артем Чжен on 03/05/23.
+//
+
+import SpriteKit
+//import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+ 
+        }
+        
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
