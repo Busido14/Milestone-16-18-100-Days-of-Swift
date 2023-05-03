01eb1c9e3d24d030c10d492e87a28ac7d9c56047
Artem <artchzhen@gmail.com>
2023-05-03 19:25:59 +0900

    added background, scoreLabel

--- Milsetone 16- 18/Milsetone 16- 18/GameScene.swift
+++ Milsetone 16- 18/Milsetone 16- 18/GameScene.swift
@@ -8,11 +8,36 @@
 import SpriteKit
 //import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var background: SKSpriteNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
     
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+
         
+        background = SKSpriteNode(imageNamed: "background")
+        background.xScale = 1.8
+        background.yScale = 1.8
+        background.position = CGPoint(x: 500, y: 400 )
+        background.zPosition = -1
+        addChild(background)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 1000, y: 700)
+        scoreLabel.horizontalAlignmentMode = .right
+        addChild(scoreLabel)
+        
+        score = 0
+        
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
  
         }
         
